28e7a32d1757978be869b50c7d80071312d1f9c2
ajaybenadict <ajayantonyraju@gmail.com>
2021-02-10 11:57:57 -0500

    recipe meal app created

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  if (!isFontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setIsFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const CategoriesScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The CategoriesScreen is working!</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoriesScreen;
